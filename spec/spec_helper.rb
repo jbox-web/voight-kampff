ae183e84b7d4dd30c386e11b357a0941eb21e349
Nicolas Rodriguez <nico@nicoladmin.fr>
2024-09-12 04:25:46 +0200

    Add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
-require 'bundler/setup'
+require 'simplecov'
+
+# Start Simplecov
+SimpleCov.start do
+  add_filter 'spec/'
+end
+
 require 'combustion'
-require 'voight_kampff'
 require 'voight_kampff/rails'
 
 Combustion.path = 'spec/dummy'
 Combustion.initialize! :action_controller
 
 require 'rspec/rails'
-Dir['./spec/support/**/*.rb'].each { |f| require f }
 
+# Configure RSpec
 RSpec.configure do |config|
   config.color = true
   config.fail_fast = false
@@ -28,3 +33,6 @@ RSpec.configure do |config|
   # see: https://relishapp.com/rspec/rspec-core/v/3-8/docs/configuration/zero-monkey-patching-mode
   config.disable_monkey_patching!
 end
+
+# Load test helpers
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
