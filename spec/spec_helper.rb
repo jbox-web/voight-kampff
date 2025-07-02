5a6d3edfd27da98f8415bae6d37904f78bfb9c50
Nicolas Rodriguez <nico@nicoladmin.fr>
2025-07-02 03:28:26 +0200

    Switch to qlty

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'simplecov_json_formatter'
 
 # Start Simplecov
 SimpleCov.start do
+  formatter SimpleCov::Formatter::JSONFormatter
   add_filter 'spec/'
 end
 
